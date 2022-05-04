fffae49f001a3eb74f568a2908bbb498a4b24acd
sauravets <saurav.dubey.ets@gmail.com>
2022-05-04 20:05:20 +0530

    create-ui

---
+++ tic-tac-toe.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tic-Tac-toe</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<h1>Tic Tac Toe Game</h1>
+<table>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+</table>
+
+<body>
+
+</body>
+
+</html>
