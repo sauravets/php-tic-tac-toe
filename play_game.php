df44342e09ffdcc6c364209a4e678f64b4d399f5
sauravets <saurav.dubey.ets@gmail.com>
2022-05-05 20:24:56 +0530

    start-changing-ui

---
+++ play_game.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Play Game</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <form method="post" action="">
+        <h1>Play Game</h1>
+        <table>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </form>
+</body>
+
+</html>
--- tic-tac-toe.php
+++ tic-tac-toe.php
@@ -9,33 +9,24 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
+<body>
 <div class="container">
     <div class="jumbotron">
-        <h1>Tic Tac Toe Game</h1>
-        <button type="button" class="btn btn-success" id="play_btn">Play</button>
+        <h1> Welcome To Tic Tac Toe Game</h1>
     </div>
-
-    <table>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-    </table>
+    <h2>Please Enter Your Name</h2>
+<form method="POST" action="play_game.php">
+  <div class="form-group">
+    <label for="first-player">First Player</label>
+    <input type="text" class="form-control" id="first_player"  placeholder="First Player">
+  </div>
+  <div class="form-group">
+    <label for="second-player">Second Player</label>
+    <input type="text" class="form-control" id="second_player" placeholder="Second Player">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
 </div>
-
-<body>
-
 </body>
 
 </html>
